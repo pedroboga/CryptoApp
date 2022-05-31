532cf4b6acf398f39962f2ca78cff289946cd79a
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-05-31 17:19:39 -0300

    Initial setup and created the color scheme for the app

---
+++ CryptoApp/CryptoApp/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Pedro Boga on 31/05/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
